500a2dbb39e0facbd42a3d3e69af49cdf4bd6f20
Ubuntu <ubuntu@ip-172-31-26-166.ap-southeast-1.compute.internal>
2015-11-20 07:02:41 +0000

    get set working

--- ERApp/routes/index.js
+++ ERApp/routes/index.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var redis = require("redis");
+que = redis.createClient();
 
+que.on("error", function (err) {
+    console.log("Error " + err);
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' , dataname: 'Home'});
 });
 router.post('/data', function(req, res, next) {
 	var name = req.body.fname;
-  res.render('index', { title: 'DataHome' , dataname:name});
+	var last = req.body.lname;
+	var newname = null;
+	que.set("First",name, function(){
+		que.get("First",function(err,value){
+			newname = value;
+			res.render('index', { title: 'DataHome' , dataname:newname });
+		})
+	});
 });
 module.exports = router;
